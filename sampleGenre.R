d7d8dfa90db0d7d7d7267323af3b94b8c8782976
mbonczkowska <mbonczkowska@sigma.ug.edu.pl>
2016-01-25 20:47:18 +0100

    Update sampleGenre.R

--- sampleGenre.R
+++ sampleGenre.R
@@ -18,14 +18,18 @@ movies <- read.csv('u.item', sep='|',header=FALSE )
 
 colnames(movies) <- m_cols
 mlens <- merge(movies, ratings, all = TRUE)
+mlensID <- mlens
 
-
-movieGenres <- merge(moviesR, ratings, all = TRUE)
+mlensID[2] <- NULL
+mlensID[3] <- NULL
+mlensID[4] <- NULL
+mlensID <- mlens[c("movie_id", "user_id", "rating")]
 # średnia ocen filmów
-movieAdvRatings <- aggregate( movieGenres$rating, list(title = movieGenres$title),  mean)
+#movieAdvRatings <- aggregate( movieGenres$rating, list(title = movieGenres$title),  mean)
 
-movieAdvGenres <- merge(moviesR,movieAdvRatings,all =TRUE)
-movieAdvGenres
+
+
+
 mlens[1] <- NULL
 mlens[2] <- NULL
 mlens[4] <- NULL
@@ -33,57 +37,71 @@ mlens[4] <- NULL
 mlens <- mlens[c("title", "user_id", "rating")]
 
 library('reshape')
-user.title.matrix.NA <- cast(mlens, title ~ user_id, value = 'rating', fun.aggregate=mean)
-
+user.movie_id.matrix.NA <- cast(mlensID, movie_id ~ user_id, value = 'rating', fun.aggregate=mean)
 # najczesciej ocenione filmy
 mostRatingUsers <- 0
-for(j in 2:ncol(user.title.matrix.NA)){
-  mostRatingUsers[j-1] <- sum(!is.na(user.title.matrix.NA[j]))
+for(j in 2:ncol(user.movie_id.matrix.NA)){
+  mostRatingUsers[j-1] <- sum(!is.na(user.movie_id.matrix.NA[j]))
 }
 
 mostRatingUsers <-(order(mostRatingUsers,decreasing = TRUE))[1:20]
-mostRatingUsers
-colnames(user.title.matrix.NA[406])
+
+
+movieGenres <- merge(moviesR, ratings, all = TRUE)
+movieGenres <- subset(movieGenres, user_id %in% mostRatingUsers)
+
+
+# srednia najczesciej oceniajacych uzytkownikow
+movieAdvRatings <- aggregate( movieGenres$rating, list(title = movieGenres$title),  mean)
+
+movieAdvGenres <- merge(moviesR,movieAdvRatings,all =TRUE)
+
+m <- subset(movieGenres, title=="Star Wars (1977)")
+sum(m[26])/nrow(m)
+
 
 mostRatedMovies <- 0
-for(i in 1:nrow(user.title.matrix.NA)){
-  mostRatedMovies[i] <- sum(!is.na(user.title.matrix.NA[i,2:ncol(user.title.matrix.NA)]))
+for(i in 1:nrow(user.movie_id.matrix.NA)){
+  mostRatedMovies[i] <- sum(!is.na(user.movie_id.matrix.NA[i,2:ncol(user.movie_id.matrix.NA)]))
 }
 sort(mostRatedMovies,decreasing = TRUE)[1:100]
 order(mostRatedMovies,decreasing = TRUE)[1:100]
 
+
+
+
 mostRatedMovies <- order(mostRatedMovies,decreasing = TRUE)[1:100]
+
 mostRated <- 0
-mostRated <- user.title.matrix.NA[mostRatedMovies,mostRatingUsers+1]
+mostRated <- user.movie_id.matrix.NA[mostRatedMovies,mostRatingUsers+1]
+rownames(!is.na(mostRated["92"]))
 
-bestRatings405 <- row.names(mostRated[mostRated$"405" == "5",colnames(mostRated)])
+most
+bestRatings92 <- row.names(mostRated[mostRated$"92" == "5",colnames(mostRated)])
 
-bestGernres405 <- moviesR[bestRatings405,6:24]
-colSums(bestGernres405)
-usersLikes <- c(1,3,4)
+bestGernres92 <- moviesR[bestRatings92,6:24]
+mostRatedMovies
+
+colSums(bestGernres92)
+usersLikes <- c(1,2,4)
 
 meanRated <- cbind(movie_id=c(rownames(mostRated)),mean=c(rowMeans(mostRated,na.rm = TRUE)))
 
 
-rated
+new <-0
 
-nrow(mostRated)
-rated <- subset(movieAdvGenres, title %in% user.title.matrix.NA[rownames(mostRated),1])
+new <-subset(movieAdvGenres, movie_id %in% mostRatedMovies)
 
+z <- is.na(user.movie_id.matrix.NA[mostRatedMovies,93])
+newzzz <- 0
+newzzz <-subset(new, movie_id %in% mostRatedMovies[z])
+newzzz
 
-newdata <- 0
-for(j in 1:nrow(mostRated)){
-  newdata[j] <- movieAdvGenres[ which(movieAdvGenres$movie_id==row.names(mostRated[j,])),]
-}
-newdata
-movieAdvGenres[3,2] 
-
-
-colSums(rated[6:24]) #
+colSums(rated[6:24]) 
 
 chosenGenres <- c(2,3,6,9,15,16,17) 
 # Action Adventure Comedy Drama Romance Sci.Fi Thriller
-genre <- rated[6:24]
+genre <- newzzz[6:24]
 genre <- genre[chosenGenres]
 correlation <- cor(genre)
 
@@ -102,14 +120,30 @@ R_p <- function(id_movie,usersLikes, correlation, rated,genre){
   return (bsum / length(usersLikes))
 }
 R_pAll <- 0
-for(i in 1:nrow(rated)){
-  R_pAll[i] <- R_p(i,usersLikes, correlation, rated,genre)
+for(i in 1:nrow(newzzz)){
+  R_pAll[i] <- R_p(i,usersLikes, correlation, newzzz,genre)
 }
-
+sort(R_pAll,decreasing = TRUE)
 
 R_pAllOrder <- order(R_pAll,decreasing = TRUE)
 
-rated[R_pAllOrder[1:10],1]
+newzzz[R_pAllOrder,1]
+newzzz[1]
+is.na(user.movie_id.matrix.NA["92"])
+mostRatedMovies
+user.movie_id.matrix.NA[,93]
 
+z <- is.na(user.movie_id.matrix.NA[mostRatedMovies,93])
+user.movie_id.matrix.NA[mostRatedMovies,93]
+mostRatedMovies[z]
+user.movie_id.matrix.NA[row.names(new),93]
+TR <- R_pAllOrder[z==TRUE]
+TR
+new[mostRatedMovies[z],1]
+newzz <-subset(user.movie_id.matrix.NA, movie_id %in% mostRatedMovies[z])
 
-
+new[newzzz[z==TRUE],1]
+new[TR,1]
+newzzz[R_pAll[1:10]]
+user.movie_id.matrix.NA[R_pAllOrder[z==1],1]
+R_pAllOrder
